fb10eb586e9e38bb6f1439bf41fff9adc3eafe6a
Kushan Joshi <0o3ko0@gmail.com>
2020-12-23 16:43:53 -0800

    add jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    testRunner: 'jest-circus/runner',
+    testPathIgnorePatterns: [
+      `<rootDir>/.yarn`,
+    ],
+    transformIgnorePatterns: ['node_modules/(?!(@popperjs)/)'],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    coveragePathIgnorePatterns: [
+      `<rootDir>/.yarn`,
+    ],
+    // collectCoverage: true,
+    clearMocks: true,
+    moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/_scripts/fileMock.js',
+      '\\.(css)$': '<rootDir>/_scripts/styleMock.js',
+    },
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+  };
+  
