c73fd91a402e21560940c3388caf28744017b29f
Kushan Joshi <0o3ko0@gmail.com>
2021-02-23 17:41:15 +0530

    Collapse of headings

--- app/markdown/parsers.js
+++ app/markdown/parsers.js
@@ -3,6 +3,9 @@ import { defaultMarkdownItTokenizer } from '@bangle.dev/markdown/index';
 import { emojiMarkdownItPlugin } from '@bangle.dev/emoji/index';
 import { specRegistry } from 'bangle-io/app/editor/spec-sheet';
 import { frontMatterMarkdownItPlugin } from '@bangle.dev/markdown-front-matter';
+import { findChildren } from '@bangle.dev/core/prosemirror/utils';
+import { Fragment, Slice } from '@bangle.dev/core/prosemirror/model';
+import { flattenFragmentJSON } from '@bangle.dev/core/components/heading';
 
 const parser = markdown.markdownParser(
   specRegistry,
@@ -14,4 +17,61 @@ const parser = markdown.markdownParser(
 const serializer = markdown.markdownSerializer(specRegistry);
 
 export const markdownParser = (markdownStr) => parser.parse(markdownStr);
-export const markdownSerializer = (doc) => serializer.serialize(doc);
+export const markdownSerializer = (doc) => {
+  const collapsedHeadingSet = new Set(
+    listCollapsedHeading(doc).map((r) => r.node),
+  );
+
+  let text;
+
+  if (collapsedHeadingSet.size === 0) {
+    console.log('nup');
+    text = serializer.serialize(doc);
+    return text;
+  }
+
+  let frag = Fragment.empty;
+
+  doc.forEach((node) => {
+    if (!collapsedHeadingSet.has(node)) {
+      frag = frag.append(Fragment.from(node));
+      return;
+    }
+
+    frag = frag
+      .append(
+        Fragment.from(
+          specRegistry.schema.nodes.heading.createChecked(
+            {
+              ...node.attrs,
+              collapseContent: null,
+            },
+            node.content,
+          ),
+        ),
+      )
+      .append(
+        Fragment.fromJSON(
+          specRegistry.schema,
+          flattenFragmentJSON(node.attrs.collapseContent),
+        ),
+      );
+  });
+
+  doc = doc.replace(0, doc.content.size, new Slice(frag, 0, 0));
+
+  text = serializer.serialize(doc);
+
+  return text;
+};
+
+// TODO need to improve this
+function listCollapsedHeading(doc) {
+  return findChildren(
+    doc,
+    (node) =>
+      node.type === specRegistry.schema.nodes.heading &&
+      Boolean(node.attrs.collapseContent),
+    false,
+  );
+}
