c04f3253ad9f1faba4af6c41c4c3f8b941348a57
Kushan Joshi <0o3ko0@gmail.com>
2021-04-17 14:04:22 -0400

    move out root home page

--- app/AppContainer.js
+++ app/AppContainer.js
@@ -14,6 +14,7 @@ import { FileBrowser } from './components/FileBrowser';
 import { OptionsBar } from './components/OptionsBar';
 import { keybindings } from 'config/index';
 import { EditorArea } from './editor/EditorArea';
+import { RootHomePage } from './components/RootHomePage';
 
 export function AppContainer() {
   const { widescreen } = useContext(UIManagerContext);
@@ -45,106 +46,6 @@ export function AppContainer() {
         </Route>
       </div>
     </>
-  );
-}
-
-function RootHomePage() {
-  const { dispatch } = useContext(UIManagerContext);
-  return (
-    <div className="flex flex-col justify-center align-middle w-8/12 overflow-y-auto homepage">
-      <h3 className="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-8">
-        bangle.io<sup className="font-light">alpha</sup>
-      </h3>
-
-      <ul className="list-inside list-disc my-2">
-        <li className="text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-1">
-          A fully <span className="font-bold">local</span> Markdown Editor - no
-          servers.
-        </li>
-        <li className="text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-1">
-          WYSIWG editor that edits markdown files saved in your hard drive
-        </li>
-        <li className="text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-1">
-          You own your data, nothing leaves your computer.
-        </li>
-      </ul>
-
-      {Boolean(window.showDirectoryPicker) ? (
-        <button
-          onClick={() => {
-            dispatch({
-              type: 'UI/CHANGE_PALETTE_TYPE',
-              value: { type: COMMAND_PALETTE, initialQuery: 'file system' },
-            });
-          }}
-          className="w-full mt-6 sm:w-auto flex-none bg-gray-800 hover:bg-pink-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-        >
-          Open a local directory that has markdown content*
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            dispatch({
-              type: 'UI/CHANGE_PALETTE_TYPE',
-              value: { type: COMMAND_PALETTE, initialQuery: 'new workspace' },
-            });
-          }}
-          className="w-full mt-6 sm:w-auto flex-none bg-gray-800 hover:bg-pink-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-        >
-          Create a workspace in your browser
-        </button>
-      )}
-
-      <button
-        onClick={() => {
-          dispatch({
-            type: 'UI/CHANGE_PALETTE_TYPE',
-            value: { type: COMMAND_PALETTE, initialQuery: 'import workspace' },
-          });
-        }}
-        className="w-full mt-6 sm:w-auto flex-none bg-gray-800 hover:bg-purple-600 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
-      >
-        Open a public Github repo with markdown content
-      </button>
-
-      <ul className="list-inside list-disc my-2">
-        After opening a workspace use:
-        <li className="text-lg sm:leading-10 font-medium mb-10 sm:mb-1">
-          Press <kbd>{keybindings.toggleFilePalette.displayValue}</kbd> for File
-          Palette
-        </li>
-        <li className="text-lg sm:leading-10 font-medium mb-10 sm:mb-1">
-          Press <kbd>{keybindings.toggleCommandPalette.displayValue}</kbd> for
-          Command Palette
-        </li>
-      </ul>
-      <p className="text-base my-2 text-sm font-semibold">
-        * Only available on Chrome and Edge
-      </p>
-      <p className="text-base my-2 text-sm font-semibold">
-        ^Please backup your data using a vcs like Git before using it.
-      </p>
-      <p className="text-base mt-3 my-2 text-lg font-semibold align-middle">
-        This is still WIP, for Issues/thoughts/❤️ please visit{' '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="text-gray-700 font-extrabold hover:underline"
-          href="https://github.com/bangle-io/bangle-io-issues"
-        >
-          Github
-        </a>
-        {' or '}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          className="text-indigo-500 font-extrabold hover:underline"
-          href="https://discord.gg/GvvbWJrVQY"
-        >
-          Discord
-        </a>
-      </p>
-    </div>
   );
 }
 
