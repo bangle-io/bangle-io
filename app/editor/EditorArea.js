7eaf695e0ed774687393223ed52c70c901417e35
Kushan Joshi <0o3ko0@gmail.com>
2021-04-18 11:41:27 -0400

    remove freeze of file not found

--- app/editor/EditorArea.js
+++ app/editor/EditorArea.js
@@ -1,37 +1,53 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { CloseIcon } from 'ui-components';
-import { UIManagerContext } from 'ui-context';
 import { cx } from 'utils/index';
 import { checkFileExists, resolvePath } from 'workspace/index';
 import { Editor } from './Editor';
 import { EmptyEditorPage } from './EmptyEditorPage';
-export function EditorArea({
-  className,
-  editorId,
-  showTabs,
-  wsPath,
-  grabFocus,
-  onClose,
-}) {
+
+/**
+ * This exists to save a render cycle
+ * when incomingWsPath changes to something else
+ * and while `checkFileExists` is doing its thing
+ * we let the previous wsPath stay, to avoid flash.
+ * @param {*} incomingWsPath
+ * @returns
+ */
+function useHandleWsPath(incomingWsPath) {
+  const [wsPath, updateWsPath] = useState(undefined);
   const [fileExists, updateFileExists] = useState(undefined);
+
   useEffect(() => {
     let destroyed = false;
-    if (wsPath) {
-      updateFileExists(false);
-      checkFileExists(wsPath).then((r) => {
-        if (r === true && !destroyed) {
-          updateFileExists(true);
-        }
-        if (r === false && !destroyed) {
-          updateFileExists(false);
+    if (incomingWsPath) {
+      checkFileExists(incomingWsPath).then((r) => {
+        if (!destroyed) {
+          updateFileExists(r);
+          updateWsPath(incomingWsPath);
         }
       });
+    }
+    if (incomingWsPath == null && wsPath) {
+      updateFileExists(undefined);
+      updateWsPath(undefined);
     }
     return () => {
       destroyed = true;
     };
-  }, [wsPath]);
+  }, [incomingWsPath, wsPath]);
 
+  return { fileExists, wsPath };
+}
+
+export function EditorArea({
+  className,
+  editorId,
+  showTabs,
+  wsPath: incomingWsPath,
+  grabFocus,
+  onClose,
+}) {
+  const { fileExists, wsPath } = useHandleWsPath(incomingWsPath);
   return (
     <div className={cx('bangle-editor-area', className)}>
       {wsPath && showTabs ? <Tab wsPath={wsPath} onClose={onClose} /> : null}
