0e455cfc4d2661bcbaf620168153e4684859cb35
Kushan Joshi <0o3ko0@gmail.com>
2021-04-28 18:38:25 -0400

    first stab

---
+++ app-vite/src/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'app/Main';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root'),
+// );
---
+++ app-vite/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  // esbuild: {
+  //   loader: {
+  //     '.js': 'jsx',
+  //   },
+  // },
+});
