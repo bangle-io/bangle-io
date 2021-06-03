41c50b7cfa65529b36fb8a26d44e33db94eacb1d
Kushan Joshi <0o3ko0@gmail.com>
2021-06-02 22:07:30 -0700

    tweak timings

--- worker/naukar-worker/collab-manager.js
+++ worker/naukar-worker/collab-manager.js
@@ -2,12 +2,14 @@ import { DebouncedDisk } from '@bangle.dev/disk';
 import { Manager } from '@bangle.dev/collab-server';
 import { getFileLastModified, getNote, saveNote } from 'workspace/index';
 
+const SLOW_FACTOR = 1;
+
 export function setupCollabManager(bangleIOContext) {
   const manager = new Manager(bangleIOContext.specRegistry.schema, {
     disk: localDisk(bangleIOContext),
-    collectUsersTimeout: 400,
-    userWaitTimeout: 250,
-    instanceCleanupTimeout: 500,
+    collectUsersTimeout: SLOW_FACTOR * 700,
+    userWaitTimeout: SLOW_FACTOR * 500,
+    instanceCleanupTimeout: SLOW_FACTOR * 2000,
   });
   return manager;
 }
