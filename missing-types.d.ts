ea79842347e48e6191fdab61e6f944c15fb9e086
Kushan Joshi <0o3ko0@gmail.com>
2021-06-17 13:38:28 -0700

    dot files

---
+++ missing-types.d.ts
@@ -0,0 +1,7 @@
+declare module '@bangle.dev/core' {
+  export const SpecRegistry: any;
+}
+
+declare module '@bangle.dev/markdown-front-matter' {
+  export const frontMatterMarkdownItPlugin: any;
+}
