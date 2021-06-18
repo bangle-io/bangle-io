0e3b1a16d4d036894718df62b68ae54a4b1f0c46
Kushan Joshi <0o3ko0@gmail.com>
2021-06-17 21:54:44 -0700

    move things in e2e (#98)
    * move things in e2e
    
    * fix

--- tooling/e2e/core/bangle-help.test.js
+++ tooling/e2e/core/bangle-help.test.js
@@ -1,4 +1,4 @@
-const { sleep, url, SELECTOR_TIMEOUT } = require('./helpers');
+const { SELECTOR_TIMEOUT, sleep, url } = require('../helpers');
 
 beforeEach(async () => {
   await jestPuppeteer.resetPage();
