adab350cb611a0ee942c1e2cb69d37dd6e4c31a5
Kushan Joshi <0o3ko0@gmail.com>
2021-11-07 16:40:59 -0500

    fix

--- app/app-entry/pages/EmptyEditorPage.tsx
+++ app/app-entry/pages/EmptyEditorPage.tsx
@@ -15,7 +15,7 @@ export function EmptyEditorPage() {
     new Set([...recentWsPaths, ...(noteWsPaths || [])].slice(0, MAX_ENTRIES)),
   );
   return (
-    <Page className="overflow-y-scroll">
+    <Page className="">
       <div className="flex flex-row">
         <h1 className="mr-1 text-3xl sm:text-2xl lg:text-3xl">{wsName}</h1>
         <ButtonIcon
@@ -60,7 +60,7 @@ export function EmptyEditorPage() {
               name: 'action::bangle-io-core-actions:NEW_NOTE_ACTION',
             });
           }}
-          className="flex-none w-full px-6 py-3 mt-6 mb-3 ml-3 text-lg font-semibold leading-6 text-white transition-colors duration-200 bg-gray-800 border border-transparent  sm:w-auto hover:bg-gray-600 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none"
+          className="flex-none w-full px-6 py-3 mt-6 mb-3 ml-3 text-lg font-semibold leading-6 text-white transition-colors duration-200 bg-gray-800 border border-transparent sm:w-auto hover:bg-gray-600 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none"
         >
           Create a note
         </button>
