89ad822e1f2092ae5534b4fb39b1fff9b73b1332
Kushan Joshi <0o3ko0@gmail.com>
2021-11-21 02:26:11 -0500

    add dau counter

--- app/app-entry/entry.tsx
+++ app/app-entry/entry.tsx
@@ -12,6 +12,7 @@ import {
 } from '@bangle.io/extension-registry';
 import { initExtensionRegistry, polyfills } from '@bangle.io/shared';
 import { UIManager } from '@bangle.io/ui-context';
+import { useLocalStorage } from '@bangle.io/utils';
 import { WorkerSetup } from '@bangle.io/worker-setup';
 import { WorkspaceContextProvider } from '@bangle.io/workspace-context';
 
@@ -59,6 +60,43 @@ export function Entry() {
       window.removeEventListener('appinstalled', appInstalledCb);
     };
   }, []);
+
+  const [lastOpened, updateLastOpened] = useLocalStorage<number | undefined>(
+    'entry-lao-2',
+    undefined,
+  );
+
+  const [dauCount, updateDauCount] = useLocalStorage<number | undefined>(
+    'entry-dau-2',
+    0,
+  );
+  console.debug({ d: dauCount });
+
+  useEffect(() => {
+    if (dauCount === 3) {
+      (window as any).fathom?.trackGoal('9MRJUARY', 1);
+    }
+    if (dauCount === 5) {
+      (window as any).fathom?.trackGoal('EWFOWT8V', 1);
+    }
+  }, [dauCount, updateDauCount]);
+
+  useEffect(() => {
+    // use local storage bug where it doesnt save
+    // info in storage initially
+    if (lastOpened === undefined) {
+      updateLastOpened(Date.now());
+    }
+
+    if (lastOpened && Date.now() - lastOpened > 60 * 1000) {
+      console.debug('updated');
+      updateLastOpened(Date.now());
+      updateDauCount((dauCount = 0) => dauCount + 1);
+
+      (window as any).fathom?.trackGoal('EC54OGMM', 1);
+    }
+  }, [lastOpened, updateDauCount, updateLastOpened]);
+
   return (
     <React.StrictMode>
       <LoadingBlock>
