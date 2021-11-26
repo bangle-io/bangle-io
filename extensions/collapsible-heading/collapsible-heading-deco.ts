48fc0bdfb586fb22ee4a93e8f3d1fbd94d05ebb5
Kushan Joshi <0o3ko0@gmail.com>
2021-11-26 15:25:51 -0500

    minor impr to decos

--- extensions/collapsible-heading/collapsible-heading-deco.ts
+++ extensions/collapsible-heading/collapsible-heading-deco.ts
@@ -15,7 +15,7 @@ export function pluginsFactory() {
   const plugin = new PluginKey(name);
 
   return [
-    new Plugin({
+    new Plugin<DecorationSet>({
       key: plugin,
       view: () => ({}),
       state: {
@@ -23,7 +23,12 @@ export function pluginsFactory() {
           return buildDeco(state);
         },
         apply(tr, old, oldState, newState) {
-          return tr.docChanged ? buildDeco(newState) : old;
+          // console.log(old, old.map(tr.mapping, tr.doc));
+          if (!tr.docChanged) {
+            return old;
+          }
+
+          return buildDeco(newState);
         },
       },
       props: {
@@ -60,47 +65,53 @@ function buildDeco(state) {
   }
 
   const decos = headings.map((match) => {
-    return Decoration.widget(match.pos + 1, (view) => {
-      let wrapper = document.createElement('span');
-      wrapper.className = 'deco-collapse-positioner';
-      // wrapper.setAttribute('data-bangle-pos', match.pos + '');
-      const child = document.createElement('span');
-      child.className = 'deco-collapse';
+    return Decoration.widget(
+      match.pos + 1,
+      (view) => {
+        let wrapper = document.createElement('span');
+        wrapper.className = 'deco-collapse-positioner';
+        // wrapper.setAttribute('data-bangle-pos', match.pos + '');
+        const child = document.createElement('span');
+        child.className = 'deco-collapse';
 
-      wrapper.appendChild(child);
+        wrapper.appendChild(child);
 
-      const svg = getNode('svg', {
-        viewBox: '0 0 24 24',
-      });
+        const svg = getNode('svg', {
+          viewBox: '0 0 24 24',
+        });
 
-      svg.addEventListener('click', function (e) {
-        const tr = view.state.tr;
+        svg.addEventListener('click', function (e) {
+          const tr = view.state.tr;
 
-        view.dispatch(
-          tr.setSelection(Selection.near(tr.doc.resolve(match.pos))),
+          view.dispatch(
+            tr.setSelection(Selection.near(tr.doc.resolve(match.pos))),
+          );
+
+          let result = heading.toggleHeadingCollapse()(view.state);
+
+          if (result) {
+            heading.toggleHeadingCollapse()(view.state, view.dispatch, view);
+            e.preventDefault();
+          }
+        });
+
+        child.appendChild(svg);
+
+        svg.setAttribute('class', 'deco-collapse-child');
+
+        svg.appendChild(
+          getNode('path', {
+            d: match.collapsed
+              ? 'M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z'
+              : 'M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z',
+          }),
         );
-
-        let result = heading.toggleHeadingCollapse()(view.state);
-
-        if (result) {
-          heading.toggleHeadingCollapse()(view.state, view.dispatch, view);
-          e.preventDefault();
-        }
-      });
-
-      child.appendChild(svg);
-
-      svg.setAttribute('class', 'deco-collapse-child');
-
-      svg.appendChild(
-        getNode('path', {
-          d: match.collapsed
-            ? 'M10.5858 6.34317L12 4.92896L19.0711 12L12 19.0711L10.5858 17.6569L16.2427 12L10.5858 6.34317Z'
-            : 'M6.34317 7.75732L4.92896 9.17154L12 16.2426L19.0711 9.17157L17.6569 7.75735L12 13.4142L6.34317 7.75732Z',
-        }),
-      );
-      return wrapper;
-    });
+        return wrapper;
+      },
+      {
+        key: match.pos + (match.collapsed ? 'true' : 'false'),
+      },
+    );
   });
 
   return DecorationSet.create(state.doc, decos);
--- extensions/editor-core/active-node.ts
+++ extensions/editor-core/active-node.ts
@@ -1,21 +1,35 @@
-import { Decoration, DecorationSet, Plugin } from '@bangle.dev/pm';
+import { Decoration, DecorationSet, EditorState, Plugin } from '@bangle.dev/pm';
 
 export function activeNode() {
   return new Plugin({
+    state: {
+      init(_, state) {
+        return buildDeco(state);
+      },
+      apply(tr, old, oldState, newState) {
+        if (!tr.selectionSet) {
+          return old;
+        }
+        return buildDeco(newState);
+      },
+    },
     props: {
       decorations(state) {
-        const selection = state.selection;
-        const resolved = selection.$from;
-
-        const decorations = selection.empty
-          ? [
-              Decoration.node(resolved.before(), resolved.after(), {
-                class: 'selected-node',
-              }),
-            ]
-          : [];
-        return DecorationSet.create(state.doc, decorations);
+        return this.getState(state);
       },
     },
   });
+}
+
+function buildDeco(state: EditorState): DecorationSet {
+  const selection = state.selection;
+  const resolved = selection.$from;
+  const decorations = selection.empty
+    ? [
+        Decoration.node(resolved.before(), resolved.after(), {
+          class: 'selected-node',
+        }),
+      ]
+    : [];
+  return DecorationSet.create(state.doc, decorations);
 }
