d38c984e56cebd2971bdbfdd317e32b2563f30f3
Kushan Joshi <0o3ko0@gmail.com>
2021-12-23 15:37:46 -0500

    add ci

---
+++ tooling/playwright-e2e/basic/dummy.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test';
+
+test('basic test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
