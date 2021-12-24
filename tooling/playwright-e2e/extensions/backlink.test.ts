fe776e1488a40c4674e24d4e575ac77ada3a5dfb
Kushan Joshi <0o3ko0@gmail.com>
2021-12-24 12:08:15 -0500

    improve flakiness

--- tooling/playwright-e2e/extensions/backlink.test.ts
+++ tooling/playwright-e2e/extensions/backlink.test.ts
@@ -4,7 +4,7 @@ import {
   clearEditor,
   createNewNote,
   createWorkspace,
-  getPrimaryEditorHandler,
+  getEditorLocator,
   runAction,
   SELECTOR_TIMEOUT,
   waitForEditorTextToContain,
@@ -18,14 +18,17 @@ test.describe.parallel('backlink workflow', () => {
   const setup = async (page: Page): Promise<string> => {
     let wsName = await createWorkspace(page);
 
-    await createNewNote(page, wsName, 'note-0');
+    const wsPath = await createNewNote(page, wsName, 'note-0');
 
-    await getPrimaryEditorHandler(page, { focus: true });
+    const editorLocator = await getEditorLocator(page, 0, {
+      focus: true,
+      wsPath,
+    });
+
     await clearEditor(page, 0);
 
     // create note-1
-    await page.keyboard.type('this is the zeroth note ');
-
+    await page.keyboard.type('this is the zeroth note ', { delay: 10 });
     await page.keyboard.type('[[note-1]]', { delay: 30 });
 
     await waitForEditorTextToContain(page, 0, 'note-1');
@@ -48,36 +51,28 @@ test.describe.parallel('backlink workflow', () => {
   };
 
   test('Creating', async ({ page }) => {
-    test.slow();
     const wsName = await setup(page);
     // make sure the backlink created is for note-0
-    expect(
-      await page.$eval(
-        '.inline-backlink_backlink',
-        (n) => (n as HTMLElement).innerText,
-      ),
-    ).toEqual('note-0');
+
+    await expect(page.locator('.inline-backlink_backlink')).toHaveText(
+      'note-0',
+      { useInnerText: true },
+    );
   });
 
   test('Hovering and clicking works', async ({ page }) => {
-    test.slow();
-
     const wsName = await setup(page);
     // make sure we are on note-1's page
     expect(await page.url()).toContain('note-1');
     // // Hover to see if it is correctly shown
     await page.hover('.inline-backlink_backlink');
 
-    await page.waitForSelector('.editor_editor-display-popup', {
-      timeout: SELECTOR_TIMEOUT,
-    });
+    await page.locator('.editor_editor-display-popup').waitFor();
 
-    expect(
-      await page.$eval(
-        '.editor_editor-display-popup',
-        (el) => (el as HTMLElement).innerText,
-      ),
-    ).toContain('this is the zeroth note note-1');
+    await expect(page.locator('.editor_editor-display-popup')).toHaveText(
+      /this is the zeroth note note-1/,
+      { useInnerText: true },
+    );
 
     // // This block exists because I was unable to click using regular way
     const popupPara = await page.$(
@@ -104,71 +99,31 @@ test.describe.parallel('backlink workflow', () => {
   });
 
   test('note widget shows backlinks', async ({ page }) => {
-    test.slow();
+    // test.slow();
 
     const wsName = await setup(page);
     // make sure we are on note-1's page
     expect(await page.url()).toContain('note-1');
+
     await runAction(
       page,
       'action::bangle-io-core-actions:NOTE_TOGGLE_SIDEBAR_ACTION',
     );
 
-    await page.waitForSelector(`.inline-backlink_widget-container`, {
-      timeout: SELECTOR_TIMEOUT,
-    });
-
-    await page.waitForFunction(
-      () =>
-        (
-          document.querySelector(
-            '.inline-backlink_widget-container',
-          ) as HTMLElement
-        )?.innerText
-          .trim()
-          .includes('note-0'),
-      {
-        timeout: 4 * SELECTOR_TIMEOUT,
-      },
-    );
-
-    expect(
-      await page.$eval(
-        '.inline-backlink_widget-container',
-        (el) => (el as HTMLElement).innerText,
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(
+        `.inline-backlink_widget-container [data-id="${wsName}:note-0.md"]`,
       ),
-    ).toContain('note-0');
-
-    await page.click(
-      `.inline-backlink_widget-container [data-id="${wsName}:note-0.md"]`,
-    );
-
-    await page.waitForFunction(
-      () => window.location.pathname.includes('note-0'),
-      {
-        timeout: 4 * SELECTOR_TIMEOUT,
-      },
-    );
-
-    // Click triggers navigation
-    await page.waitForLoadState('networkidle');
+    ]);
 
     expect(await page.url()).toContain('note-0');
   });
 });
 
 async function clickBacklinkHandle(page: Page, text: string) {
-  const editorHandle = await getPrimaryEditorHandler(page);
   await Promise.all([
-    page.waitForNavigation({
-      timeout: 5000,
-      waitUntil: 'networkidle',
-    }),
-    editorHandle?.$$eval(
-      '.inline-backlink_backlink',
-      (nodes: HTMLElement[], text) =>
-        [...nodes].find((n) => n?.innerText === text)?.click(),
-      text,
-    ),
+    page.waitForNavigation(),
+    page.click(`.bangle-editor button:has-text("${text}")`),
   ]);
 }
