26a0a4b0816c2719157c2079261790b557f28997
Kushan Joshi <0o3ko0@gmail.com>
2021-12-26 12:07:51 -0500

    cleanup types

--- lib/shared-types/index.d.ts
+++ lib/shared-types/index.d.ts
@@ -1,7 +1,6 @@
 export * from './editor';
 export * from './extension-registry';
 export * from './helper';
-export * from './page-context';
 export * from './store';
 export * from './theme';
 export * from './ui';
--- lib/shared-types/page-context.ts
+++
@@ -1,6 +0,0 @@
-import type { PAGE_BLOCK_RELOAD_ACTION_NAME } from '@bangle.io/constants';
-
-export type PAGE_BLOCK_RELOAD_ACTION_TYPE = {
-  name: typeof PAGE_BLOCK_RELOAD_ACTION_NAME;
-  value: boolean;
-};
