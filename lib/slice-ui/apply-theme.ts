351abc90a6c99b1f208a6ecdd7b2f5aadb62cf0e
Kushan Joshi <0o3ko0@gmail.com>
2022-03-31 00:07:46 -0400

    Feat/improve dark mode (#312)
    * introduce main.css
    
    * improve dark mode

--- lib/slice-ui/apply-theme.ts
+++ lib/slice-ui/apply-theme.ts
@@ -1,19 +1,7 @@
 import type { ThemeType } from '@bangle.io/shared-types';
 
-import { cssVars } from './css-vars';
-
 export function applyTheme(theme: ThemeType) {
   console.debug('applying theme', theme);
-  const element = document.documentElement;
-  for (const cssVar of cssVars) {
-    updateStyleHelper(theme, cssVar, element);
-  }
-}
 
-function updateStyleHelper(
-  theme: ThemeType,
-  cssVar: string,
-  element: HTMLElement,
-) {
-  element.style.setProperty(`--BV-${cssVar}`, `var(--BV-${theme}-${cssVar})`);
+  document.documentElement.setAttribute('data-theme', theme);
 }
