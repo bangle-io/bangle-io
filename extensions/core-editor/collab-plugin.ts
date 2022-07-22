62976908c0f3928b6f6d2df4e1bff382df92090c
Kushan Joshi <0o3ko0@gmail.com>
2022-07-21 22:20:48 -0400

    add warmup time in collab-client

--- extensions/core-editor/collab-plugin.ts
+++ extensions/core-editor/collab-plugin.ts
@@ -2,17 +2,15 @@ import { collabClient } from '@bangle.dev/collab-client';
 import { uuid } from '@bangle.dev/utils';
 
 import type { EditorPluginMetadata } from '@bangle.io/shared-types';
-
-import { coreEditorSliceKey } from './core-editor-slice';
+import { getCollabMessageBus } from '@bangle.io/slice-editor-sync';
 
 export function collabPlugin({ metadata }: { metadata: EditorPluginMetadata }) {
   return collabClient.plugins({
     docName: metadata.wsPath,
     clientID: 'client-' + metadata.editorId + '-' + uuid(4),
-    collabMessageBus: coreEditorSliceKey.getSliceStateAsserted(
-      metadata.bangleStore.state,
-    ).collabMessageBus,
-    cooldownTime: 150,
+    collabMessageBus: getCollabMessageBus()(metadata.bangleStore.state),
+    cooldownTime: 550,
     requestTimeout: 1000,
+    warmupTime: 20,
   });
 }
