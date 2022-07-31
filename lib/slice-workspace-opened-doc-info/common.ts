e9249799a1e0adb1e68a3147842ef7aea9316741
Kushan Joshi <0o3ko0@gmail.com>
2022-07-30 19:03:40 -0700

    add comments

--- lib/slice-workspace-opened-doc-info/common.ts
+++ lib/slice-workspace-opened-doc-info/common.ts
@@ -1,11 +1,19 @@
 import { SliceKey } from '@bangle.io/create-store';
 
+// The idea of this slice is to store information related to a opened files sha.
+// And then use that information to determine if the file was changed externally (outside of the application).
+// Note: the slice serves as a common place to store this information and provides api
+// to modify this information, but not on how and when to update this information which
+// is someone elses responsibility.
 export interface OpenedFile {
   readonly wsPath: string;
   readonly pendingWrite: boolean;
   readonly sha?: string | null;
+  // the sha representing the sha currently on the disk. The faster this is updated, the faster
+  // we can react to an external modification a file.
   readonly currentDiskSha?: string | null;
-  // The last sha we saw on disk either during writing or reading
+  // The last sha our application wrote to the disk or read if the file was not modified.
+  // Comparing this to currentDiskSha will determine if the file was modified externally.
   readonly lastKnownDiskSha?: string | null;
   readonly currentDiskShaTimestamp?: number | null;
 }
