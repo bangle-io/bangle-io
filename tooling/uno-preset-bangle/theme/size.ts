87fc111cab56df76eea546989452971f20f17ea1
Kushan Joshi <0o3ko0@gmail.com>
2022-12-17 18:38:12 -0500

    fix size in uno

--- tooling/uno-preset-bangle/theme/size.ts
+++ tooling/uno-preset-bangle/theme/size.ts
@@ -1,31 +1,29 @@
 import { vars } from '@bangle.io/atomic-css';
 
-const baseSize = vars.size;
-
 export const width = {
   auto: 'auto',
-  ...baseSize,
+  ...vars.space,
   screen: '100vw',
 };
 
 export const maxWidth = {
   none: 'none',
-  ...baseSize,
+  ...vars.space,
   screen: '100vw',
 };
 
 export const height = {
   auto: 'auto',
-  ...baseSize,
+  ...vars.space,
   screen: '100vh',
 };
 
 export const maxHeight = {
   none: 'none',
-  ...baseSize,
+  ...vars.space,
   screen: '100vh',
 };
 
 export const containers = Object.fromEntries(
-  Object.entries(baseSize).map(([k, v]) => [k, `(min-width: ${v})`]),
+  Object.entries(vars.space).map(([k, v]) => [k, `(min-width: ${v})`]),
 );
