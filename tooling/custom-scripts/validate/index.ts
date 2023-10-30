6cb2905423a520e966e6524c027eb1d961a07bcf
Kushan Joshi <0o3ko0@gmail.com>
2023-10-29 22:09:15 -0400

    save constraints (#464)
    * save constraints
    
    * break
    
    * break
    
    * break

---
+++ tooling/custom-scripts/validate/index.ts
@@ -0,0 +1,82 @@
+import { makeThrowValidationError } from '../lib';
+import { Package, setup, Workspace } from '../lib/workspace-helper';
+
+void setup().then(async (item) => {
+  void shouldRespectAllowedWorkspace(item.packagesMap, item.workspaces);
+  void shouldOnlyUseDependenciesDefinedInPackageJSON(item.packagesMap);
+  void shouldOnlyUseDevDependenciesDefinedInPackageJSON(item.packagesMap);
+});
+
+async function shouldRespectAllowedWorkspace(
+  packageMap: Map<string, Package>,
+  workspaceMap: Record<string, Workspace>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldRespectAllowedWorkspace',
+  );
+
+  for (const [name, pkg] of packageMap.entries()) {
+    let deps = Object.keys(pkg.workspaceDependencies);
+    deps.push(...Object.keys(pkg.workspaceDevDependencies));
+
+    deps = [...new Set(deps)];
+
+    for (const dep of deps) {
+      const result = pkg.workspace.allowedWsDependency(
+        [...Object.values(workspaceMap)],
+        dep,
+      );
+
+      if (!result) {
+        throwValidationError(
+          `Package ${name} imports ${dep}  
+          which is not allowed in the workspace. 
+          Check "allowedWorkspaces" in the package.json of workspace of ${pkg.workspace.path}`,
+        );
+      }
+    }
+  }
+}
+
+async function shouldOnlyUseDependenciesDefinedInPackageJSON(
+  packageMap: Map<string, Package>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldOnlyUseDependenciesDefinedInPackageJSON',
+  );
+  for (const [name, pkg] of packageMap.entries()) {
+    const deps = (
+      await pkg.getImportedPackages((file) => file.isSrcFile)
+    ).filter((dep) => !dep.startsWith('node:'));
+
+    for (const dep of deps) {
+      if (!pkg.dependencies[dep]) {
+        throwValidationError(
+          `Package ${name} ${pkg.packageJSONPath}  imports ${dep} which is not in the workspace`,
+        );
+      }
+    }
+  }
+}
+
+async function shouldOnlyUseDevDependenciesDefinedInPackageJSON(
+  packageMap: Map<string, Package>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldOnlyUseDevDependenciesDefinedInPackageJSON',
+  );
+
+  for (const [name, pkg] of packageMap.entries()) {
+    const deps = (
+      await pkg.getImportedPackages((file) => file.isTestFile)
+    ).filter((dep) => !dep.startsWith('node:'));
+
+    for (const dep of deps) {
+      if (!pkg.devDependencies?.[dep] && !pkg.dependencies[dep]) {
+        throwValidationError(
+          `Package ${name} ${pkg.packageJSONPath}  imports ${dep} which is not in the workspace`,
+        );
+      }
+    }
+  }
+}
