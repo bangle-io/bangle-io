35354f0f96535b4f49439321953634d982499c9e
Kushan Joshi <0o3ko0@gmail.com>
2023-10-29 21:55:37 -0400

    save constraints

--- tooling/custom-scripts/lib/constants.ts
+++ tooling/custom-scripts/lib/constants.ts
@@ -2,6 +2,8 @@ import { z } from 'zod';
 
 export const ALL_TOP_LEVEL_DIRS = ['app', 'js-lib', 'tooling', 'e2e-tests'];
 
-export const bangleWorkspaceConfigSchema = z.object({});
+export const bangleWorkspaceConfigSchema = z.object({
+  allowedWorkspaces: z.array(z.string()),
+});
 
 export type BangleWorkspaceConfig = z.infer<typeof bangleWorkspaceConfigSchema>;
---
+++ tooling/custom-scripts/validate/index.ts
@@ -0,0 +1,77 @@
+import { makeThrowValidationError } from '../lib';
+import { Package, setup, Workspace } from '../lib/workspace-helper';
+
+void setup().then(async (item) => {
+  void shouldRespectAllowedWorkspace(item.packagesMap, item.workspaces);
+  void shouldOnlyUseDependenciesDefinedInPackageJSON(item.packagesMap);
+  void shouldOnlyUseDevDependenciesDefinedInPackageJSON(item.packagesMap);
+});
+
+async function shouldRespectAllowedWorkspace(
+  packageMap: Map<string, Package>,
+  workspaceMap: Record<string, Workspace>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldRespectAllowedWorkspace',
+  );
+
+  for (const [name, pkg] of packageMap.entries()) {
+    const deps = Object.keys(pkg.workspaceDependencies);
+    for (const dep of deps) {
+      const result = pkg.workspace.allowedWsDependency(
+        [...Object.values(workspaceMap)],
+        dep,
+      );
+
+      if (!result) {
+        throwValidationError(
+          `Package ${name} imports ${dep}  
+          which is not allowed in the workspace`,
+        );
+      }
+    }
+  }
+}
+
+async function shouldOnlyUseDependenciesDefinedInPackageJSON(
+  packageMap: Map<string, Package>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldOnlyUseDependenciesDefinedInPackageJSON',
+  );
+  for (const [name, pkg] of packageMap.entries()) {
+    const deps = (
+      await pkg.getImportedPackages((file) => file.isSrcFile)
+    ).filter((dep) => !dep.startsWith('node:'));
+
+    for (const dep of deps) {
+      if (!pkg.dependencies[dep]) {
+        throwValidationError(
+          `Package ${name} ${pkg.packageJSONPath}  imports ${dep} which is not in the workspace`,
+        );
+      }
+    }
+  }
+}
+
+async function shouldOnlyUseDevDependenciesDefinedInPackageJSON(
+  packageMap: Map<string, Package>,
+) {
+  const throwValidationError = makeThrowValidationError(
+    'shouldOnlyUseDevDependenciesDefinedInPackageJSON',
+  );
+
+  for (const [name, pkg] of packageMap.entries()) {
+    const deps = (
+      await pkg.getImportedPackages((file) => file.isTestFile)
+    ).filter((dep) => !dep.startsWith('node:'));
+
+    for (const dep of deps) {
+      if (!pkg.devDependencies?.[dep] && !pkg.dependencies[dep]) {
+        throwValidationError(
+          `Package ${name} ${pkg.packageJSONPath}  imports ${dep} which is not in the workspace`,
+        );
+      }
+    }
+  }
+}
