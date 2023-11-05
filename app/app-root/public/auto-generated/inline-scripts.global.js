281bbb646f14cccb83ae6173979e78b2ac218a76
Kushan Joshi <0o3ko0@gmail.com>
2023-11-04 21:32:28 -0400

    fix widescreen

--- app/app-root/public/auto-generated/inline-scripts.global.js
+++ app/app-root/public/auto-generated/inline-scripts.global.js
@@ -30,4 +30,50 @@
     });
   }
 
+  // ../../shared/constants/ui.ts
+  var WIDESCREEN_WIDTH = 759;
+
+  // widescreen.ts
+  function testPlatform(re) {
+    let target = typeof globalThis !== "undefined" ? globalThis : typeof window !== "undefined" ? window : typeof global !== "undefined" ? global : {};
+    if (target.navigator != null) {
+      const platform = target.navigator.userAgentData?.platform || target.navigator.platform;
+      return re.test(platform);
+    }
+    return false;
+  }
+  var isAndroid = typeof navigator != "undefined" ? /Android \d/.test(navigator.userAgent) : false;
+  var isMac = testPlatform(/^Mac/i);
+  var isIPhone = testPlatform(/^iPhone/i);
+  var isIPad = testPlatform(/^iPad/i) || // iPadOS 13 lies and says it's a Mac, but we can distinguish by detecting touch support.
+  isMac && navigator.maxTouchPoints > 1;
+  var isIOS = isIPhone || isIPad;
+  var isMobile = isAndroid || isIOS;
+  function checkWidescreen(width = typeof window !== "undefined" ? window.innerWidth : void 0) {
+    if (isMobile) {
+      return false;
+    }
+    return width ? WIDESCREEN_WIDTH <= width : false;
+  }
+  function setRootWidescreenClass() {
+    if (typeof document === "undefined") {
+      return;
+    }
+    const widescreen = checkWidescreen();
+    setItem(document.firstElementChild);
+    function setItem(element) {
+      if (!element) {
+        return;
+      }
+      if (widescreen) {
+        element.classList.add("BU_widescreen");
+        element.classList.remove("BU_smallscreen");
+      } else {
+        element.classList.remove("BU_widescreen");
+        element.classList.add("BU_smallscreen");
+      }
+    }
+  }
+  setRootWidescreenClass();
+
 })();
--- tooling/inline-scripts/tsup.config.ts
+++ tooling/inline-scripts/tsup.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   dts: false,
   shims: false,
   clean: false,
+  minify: false,
   name: 'inline-scripts',
   outDir: path.join(appRoot, 'public', 'auto-generated'),
 });
