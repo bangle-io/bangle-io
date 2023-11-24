e279aabd4ed3d46612d7934c77ed6391fbd49b4e
Kushan Joshi <0o3ko0@gmail.com>
2023-11-24 15:21:03 -0500

    implement run immediate

--- packages/js-lib/nsm-3/index.ts
+++ packages/js-lib/nsm-3/index.ts
@@ -32,3 +32,50 @@ export const zeroTimeoutScheduler: EffectScheduler = (cb, opts) => {
     clearTimeout(id);
   };
 };
+
+// runImmediately
+
+const hasIdleCallback =
+  typeof window !== 'undefined' && 'requestIdleCallback' in window;
+
+const verified = new WeakSet<() => void | Promise<void>>();
+
+export const customBangleScheduler: EffectScheduler = (cb, opts) => {
+  const metadata = opts.metadata;
+  // TODO: remove this check once we have a more performant way
+  if (
+    Object.keys(metadata).length > 1 ||
+    (Object.keys(metadata).length === 1 && !metadata.runImmediately)
+  ) {
+    throw new Error(
+      `customBangleScheduler only supports metadata.runImmediately`,
+    );
+  }
+
+  const runImmediately = metadata.runImmediately;
+
+  if (runImmediately) {
+    queueMicrotask(() => {
+      void cb();
+    });
+    return () => {
+      // noop
+    };
+  }
+
+  if (hasIdleCallback) {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    const ref = window.requestIdleCallback(cb, {
+      timeout: opts.maxWait,
+    });
+    return () => {
+      window.cancelIdleCallback(ref);
+    };
+  } else {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    const ref = setTimeout(cb, opts.maxWait);
+    return () => {
+      clearTimeout(ref);
+    };
+  }
+};
