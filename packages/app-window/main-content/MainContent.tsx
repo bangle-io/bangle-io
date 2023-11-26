81be9a2ece4a91a30bfc009d711a47056f222a58
Kushan Joshi <0o3ko0@gmail.com>
2023-11-26 13:53:32 -0500

    add bogus data

--- packages/app-window/main-content/MainContent.tsx
+++ packages/app-window/main-content/MainContent.tsx
@@ -19,18 +19,18 @@ import React from 'react';
 import { sliceUI } from '@bangle.io/slice-ui';
 
 export function MainContent() {
-  const {} = useTrack(sliceUI);
   const store = useStore();
 
   return (
     <Flex
       direction="column"
       height="100%"
-      UNSAFE_className="overflow-y-scroll B-app-main-content px-4 pt-4"
+      UNSAFE_className="overflow-y-scroll B-app-main-content px-4 p-4"
     >
       <TableView
         aria-label="Example table with static contents"
         selectionMode="multiple"
+        height="100%"
       >
         <TableHeader>
           <Column>Name</Column>
@@ -38,6 +38,86 @@ export function MainContent() {
           <Column align="end">Date Modified</Column>
         </TableHeader>
         <TableBody>
+          <Row>
+            <Cell>Games</Cell>
+            <Cell>File folder</Cell>
+            <Cell>6/7/2020</Cell>
+          </Row>
+          <Row>
+            <Cell>Program Files</Cell>
+            <Cell>File folder</Cell>
+            <Cell>4/7/2021</Cell>
+          </Row>
+          <Row>
+            <Cell>bootmgr</Cell>
+            <Cell>System file</Cell>
+            <Cell>11/20/2010</Cell>
+          </Row>
+          <Row>
+            <Cell>log.txt</Cell>
+            <Cell>Text Document</Cell>
+            <Cell>1/18/2016</Cell>
+          </Row>
+          <Row>
+            <Cell>Games</Cell>
+            <Cell>File folder</Cell>
+            <Cell>6/7/2020</Cell>
+          </Row>
+          <Row>
+            <Cell>Program Files</Cell>
+            <Cell>File folder</Cell>
+            <Cell>4/7/2021</Cell>
+          </Row>
+          <Row>
+            <Cell>bootmgr</Cell>
+            <Cell>System file</Cell>
+            <Cell>11/20/2010</Cell>
+          </Row>
+          <Row>
+            <Cell>log.txt</Cell>
+            <Cell>Text Document</Cell>
+            <Cell>1/18/2016</Cell>
+          </Row>
+          <Row>
+            <Cell>Games</Cell>
+            <Cell>File folder</Cell>
+            <Cell>6/7/2020</Cell>
+          </Row>
+          <Row>
+            <Cell>Program Files</Cell>
+            <Cell>File folder</Cell>
+            <Cell>4/7/2021</Cell>
+          </Row>
+          <Row>
+            <Cell>bootmgr</Cell>
+            <Cell>System file</Cell>
+            <Cell>11/20/2010</Cell>
+          </Row>
+          <Row>
+            <Cell>log.txt</Cell>
+            <Cell>Text Document</Cell>
+            <Cell>1/18/2016</Cell>
+          </Row>
+          <Row>
+            <Cell>Games</Cell>
+            <Cell>File folder</Cell>
+            <Cell>6/7/2020</Cell>
+          </Row>
+          <Row>
+            <Cell>Program Files</Cell>
+            <Cell>File folder</Cell>
+            <Cell>4/7/2021</Cell>
+          </Row>
+          <Row>
+            <Cell>bootmgr</Cell>
+            <Cell>System file</Cell>
+            <Cell>11/20/2010</Cell>
+          </Row>
+          <Row>
+            <Cell>log.txt</Cell>
+            <Cell>Text Document</Cell>
+            <Cell>1/18/2016</Cell>
+          </Row>
           <Row>
             <Cell>Games</Cell>
             <Cell>File folder</Cell>
