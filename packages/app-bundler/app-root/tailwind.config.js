145e7f16ac2670d1305aee9eca12d2ee653290d8
Kushan Joshi <0o3ko0@gmail.com>
2024-02-11 14:18:21 -0500

    add some base colors

--- packages/app-bundler/app-root/tailwind.config.js
+++ packages/app-bundler/app-root/tailwind.config.js
@@ -1,12 +1,34 @@
 const path = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class', '.BU_dark-scheme'],
+
   content: {
     relative: true,
     files: ['!../../**/node_modules', '../../**/*.{ts,tsx,html}'],
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          50: 'var(--BV-gray-50)',
+          75: 'var(--BV-gray-75)',
+          100: 'var(--BV-gray-100)',
+          200: 'var(--BV-gray-200)',
+          300: 'var(--BV-gray-300)',
+          400: 'var(--BV-gray-400)',
+          500: 'var(--BV-gray-500)',
+          600: 'var(--BV-gray-600)',
+          700: 'var(--BV-gray-700)',
+          800: 'var(--BV-gray-800)',
+          900: 'var(--BV-gray-900)',
+        },
+      },
+      borderWidth: {
+        1: '1px',
+        3: '3px',
+      },
+    },
   },
   plugins: [require('tailwindcss-react-aria-components')],
 };
