f20a68b67feea7e8bc930d7bf14d56f671c5410f
Kushan Joshi <0o3ko0@gmail.com>
2024-11-26 22:44:34 -0500

    update storybok

--- packages/ui/ui-components/src/dialog-single-select.stories.tsx
+++ packages/ui/ui-components/src/dialog-single-select.stories.tsx
@@ -41,98 +41,39 @@ const meta: Meta<typeof DialogSingleSelect> = {
 
 export default meta;
 type Story = StoryObj<typeof DialogSingleSelect>;
-export function Default() {
-  return (
-    <DialogSingleSelect
-      open={true}
-      setOpen={() => {}}
-      options={[
-        { id: 'option1', title: 'option1' },
-        { id: 'Option 2', title: 'option2' },
-        { id: 'Option 3', title: 'option3' },
-      ]}
-      onSelect={() => {}}
-      placeholder="Select an option"
-    />
-  );
-}
 
-export function DefaultBadge() {
-  return (
-    <DialogSingleSelect
-      open={true}
-      setOpen={() => {}}
-      options={[
-        { id: 'option1', title: 'option1' },
-        { id: 'Option 2', title: 'option2' },
-        { id: 'Option 3', title: 'option3' },
-      ]}
-      onSelect={() => {}}
-      placeholder="Select an option"
-      badgeText="Issues"
-    />
-  );
-}
+const Template: Story = {
+  args: {
+    open: true,
+    setOpen: () => {},
+    options: [
+      { id: 'option1', title: 'option1' },
+      { id: 'Option 2', title: 'option2' },
+      { id: 'Option 3', title: 'option3' },
+    ],
+    onSelect: () => {},
+    placeholder: 'Select an option',
+  },
+};
 
-export function DestructiveBadge() {
-  return (
-    <DialogSingleSelect
-      open={true}
-      setOpen={() => {}}
-      options={[
-        { id: 'option1', title: 'option1' },
-        { id: 'Option 2', title: 'option2' },
-        { id: 'Option 3', title: 'option3' },
-      ]}
-      onSelect={() => {}}
-      placeholder="Select an option"
-      badgeText="Issues"
-      badgeTone="destructive"
-    />
-  );
-}
+export const Default: Story = {
+  ...Template,
+};
 
-export function Example() {
-  const [open, setOpen] = React.useState(true);
-  return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandBadge>Issues</CommandBadge>
-      <CommandInput placeholder="Type a command or search..." Icon={User} />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>
-            <Calendar />
-            <span>Calendar</span>
-          </CommandItem>
-          <CommandItem>
-            <Smile />
-            <span>Search Emoji</span>
-          </CommandItem>
-          <CommandItem>
-            <Calculator />
-            <span>Calculator</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <User />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
-    </CommandDialog>
-  );
-}
+export const DefaultBadge: Story = {
+  ...Template,
+  args: {
+    ...Template.args,
+    badgeText: 'Issues',
+  },
+};
+
+export const DestructiveBadge: Story = {
+  ...Template,
+  args: {
+    ...Template.args,
+    badgeText: 'Issues',
+    badgeTone: 'destructive',
+    open: true,
+  },
+};
