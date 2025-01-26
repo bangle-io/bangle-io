d2623656e3475e1b98c1d48c6b22b5f362f26839
Kushan Joshi <0o3ko0@gmail.com>
2025-01-26 17:00:41 -0500

    Update e2e test snapshots and component tests
    - Reorganized snapshot files for component tests in e2e-tests directory
    - Added new component tests for app sidebar, workspace dialog, and Dhancha components
    - Moved existing snapshots to a new 'component-tests' subdirectory
    - Cleaned up old snapshot files from previous test locations

---
+++ packages/tooling/e2e-tests/src/component-tests/app-sidebar.ct.tsx
@@ -0,0 +1,27 @@
+import { AppSidebarExample } from '@bangle.io/ui-components/src/app-sidebar.stories.tsx';
+import { expect, test } from '@playwright/experimental-ct-react';
+import React from 'react';
+
+test('Collapsing file', async ({ mount }) => {
+  const component = await mount(<AppSidebarExample />);
+
+  await component.getByText('Files').scrollIntoViewIfNeeded();
+  await component.getByText('Files').click();
+  await component.getByRole('button', { name: 'components' }).click();
+
+  await expect(component).toHaveScreenshot();
+  await component.getByRole('button', { name: 'components' }).click();
+  await expect(component).toHaveScreenshot();
+});
+
+test('Toggle sidebar', async ({ mount }) => {
+  const component = await mount(<AppSidebarExample />);
+
+  await component.getByRole('button', { name: 'Toggle Sidebar' }).click();
+
+  await expect(component).toHaveScreenshot();
+
+  await component.getByRole('button', { name: 'Toggle Sidebar' }).click();
+
+  await expect(component).toHaveScreenshot();
+});
