5ca2c8109a4d08e2f7461b8f72c1ad713a97992b
Kushan Joshi <0o3ko0@gmail.com>
2025-04-18 17:29:22 -0400

    Refactor Editor component to use useCallback for memoization
    - Updated the Editor component in `index.tsx` to import `useCallback` from React and utilize it for the ref callback, improving performance by preventing unnecessary re-renders.
    - Added comments to clarify the reason for using `useCallback` in the context of React 19 behavior changes.

--- packages/core/editor/src/index.tsx
+++ packages/core/editor/src/index.tsx
@@ -3,7 +3,7 @@ import './typography.css';
 
 import { cx } from '@bangle.io/base-utils';
 import { useCoreServices } from '@bangle.io/context';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { LinkMenu, SlashCommand } from './components';
 export { PmEditorService } from './pm-editor-service';
 
@@ -22,19 +22,24 @@ export function Editor({
     <div className="box-border flex h-full min-h-36 w-full flex-col ">
       <div className="relative box-border w-full flex-1">
         <div
-          ref={(node) => {
-            const cleanup =
-              node &&
-              pmEditorService.mountEditor({
-                domNode: node,
-                wsPath,
-                name,
-              });
+          // in react 19 callback change retriggers this callback
+          // so we use useCallback to memoize the callback
+          ref={useCallback(
+            (node: HTMLElement | null) => {
+              const cleanup =
+                node &&
+                pmEditorService.mountEditor({
+                  domNode: node,
+                  wsPath,
+                  name,
+                });
 
-            return () => {
-              cleanup?.();
-            };
-          }}
+              return () => {
+                cleanup?.();
+              };
+            },
+            [name, wsPath, pmEditorService],
+          )}
           data-editor-name={name}
           className={cx(
             'ProseMirror box-border min-h-full py-8 outline-none outline-0',
