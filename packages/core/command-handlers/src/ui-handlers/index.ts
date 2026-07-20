edc210e03ccac5f1446f5fa8c106caffca068706
Kushan Joshi <0o3ko0@gmail.com>
2026-07-20 00:24:37 -0400

    refactor(commands): remove production test handler

--- packages/core/command-handlers/src/ui-handlers/index.ts
+++ packages/core/command-handlers/src/ui-handlers/index.ts
@@ -2,5 +2,4 @@ export { basicOperationsHandlers } from './basic-operations';
 export { editorOperationsHandlers } from './editor-operations';
 export { nativeFsRecoveryHandlers } from './native-fs-recovery';
 export { noteManagementHandlers } from './note-management';
-export { testHandlers } from './test-handler';
 export { workspaceManagementHandlers } from './workspace-management';
